d9a07ce23e890672a371aed33f3943306a007500
qingliu <1136712618@qq.com>
2024-09-02 16:05:32 +0800

    cat&lab04 finish

--- homework/hw02/hw02.py
+++ homework/hw02/hw02.py
@@ -32,11 +32,11 @@ def product(n, term):
     162
     """
     "*** YOUR CODE HERE ***"
-    assert n > 0,"n must be a positive integer"
-    i ,result= 1 ,1
-    while i < n:
-        result,i = result*term(i),i+1 
-    return result
+    if n == 1:
+        return term(1)
+    else:
+        return term(n)*product(n-1,term)
+
 
 def accumulate(fuse, start, n, term):
     """Return the result of fusing together the first n terms in a sequence 
@@ -58,18 +58,20 @@ def accumulate(fuse, start, n, term):
     19
     """
     "*** YOUR CODE HERE ***"
-    assert n > 0,"fuse is never used"
-    re ,i= start ,1
-    while i <= n:
-        re,i = fuse(re,term(i)), i+1
-    return re
+    if n == 0:
+        return start
+    elif n == 1:
+        return fuse(start,term(1))
+    else:
+        return fuse(term(n),accumulate(fuse,start,n-1,term))
+
 
 def summation_using_accumulate(n, term):
     """Returns the sum: term(1) + ... + term(n), using accumulate.
 
-    >>> summation_using_accumulate(5, square)
+    >>> summation_using_accumulate(5, square) # square(0) + square(1) + ... + square(4) + square(5)
     55
-    >>> summation_using_accumulate(5, triple)
+    >>> summation_using_accumulate(5, triple) # triple(0) + triple(1) + ... + triple(4) + triple(5)
     45
     >>> # This test checks that the body of the function is just a return statement.
     >>> import inspect, ast
@@ -82,16 +84,16 @@ def summation_using_accumulate(n, term):
 def product_using_accumulate(n, term):
     """Returns the product: term(1) * ... * term(n), using accumulate.
 
-    >>> product_using_accumulate(4, square)
+    >>> product_using_accumulate(4, square) # square(1) * square(2) * square(3) * square()
     576
-    >>> product_using_accumulate(6, triple)
+    >>> product_using_accumulate(6, triple) # triple(1) * triple(2) * ... * triple(5) * triple(6)
     524880
     >>> # This test checks that the body of the function is just a return statement.
     >>> import inspect, ast
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(product_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    return accumulate(mul,0,n,term)
+    return accumulate(mul,1,n,term)
 
 
 def make_repeater(f, n):
@@ -100,7 +102,7 @@ def make_repeater(f, n):
     >>> add_three = make_repeater(increment, 3)
     >>> add_three(5)
     8
-    >>> make_repeater(triple, 5)(1) # 3 * 3 * 3 * 3 * 3 * 1
+    >>> make_repeater(triple, 5)(1) # 3 * (3 * (3 * (3 * (3 * 1))))
     243
     >>> make_repeater(square, 2)(5) # square(square(5))
     625
@@ -108,4 +110,133 @@ def make_repeater(f, n):
     390625
     """
     "*** YOUR CODE HERE ***"
+    def repeater(x):
+        for _ in range(n):
+            x = f(x)
+        return x
+    return repeater
+
+
+def digit_distance(n):
+    """Determines the digit distance of n.
+
+    >>> digit_distance(3)
+    0
+    >>> digit_distance(777)
+    0
+    >>> digit_distance(314)
+    5
+    >>> digit_distance(31415926535)
+    32
+    >>> digit_distance(3464660003)
+    16
+    >>> from construct_check import check
+    >>> # ban all loops
+    >>> check(HW_SOURCE_FILE, 'digit_distance',
+    ...       ['For', 'While'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    n, next, now = n // 10, n // 10 % 10, n % 10
+    if n == 0:
+        return 0
+    else:
+        return abs(next - now) + digit_distance(n)
+
+
+def interleaved_sum(n, odd_func, even_func):
+    """Compute the sum odd_func(1) + even_func(2) + odd_func(3) + ..., up
+    to n.
+
+    >>> identity = lambda x: x
+    >>> square = lambda x: x * x
+    >>> triple = lambda x: x * 3
+    >>> interleaved_sum(5, identity, square) # 1   + 2*2 + 3   + 4*4 + 5
+    29
+    >>> interleaved_sum(5, square, identity) # 1*1 + 2   + 3*3 + 4   + 5*5
+    41
+    >>> interleaved_sum(4, triple, square)   # 1*3 + 2*2 + 3*3 + 4*4
+    32
+    >>> interleaved_sum(4, square, triple)   # 1*1 + 2*3 + 3*3 + 4*3
+    28
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'interleaved_sum', ['While', 'For', 'Mod']) # ban loops and %
+    True
+    >>> check(HW_SOURCE_FILE, 'interleaved_sum', ['BitAnd', 'BitOr', 'BitXor']) # ban bitwise operators, don't worry about these if you don't know what they are
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    def func(k):
+        if k > n:
+            return 0
+        elif k == n:
+            return odd_func(n)
+        else:
+            return odd_func(k) + even_func(k+1) + func(k+2)
+    return func(1)
+
+
+def next_larger_coin(coin):
+    """Returns the next larger coin in order.
+    >>> next_larger_coin(1)
+    5
+    >>> next_larger_coin(5)
+    10
+    >>> next_larger_coin(10)
+    25
+    >>> next_larger_coin(2) # Other values return None
+    """
+    if coin == 1:
+        return 5
+    elif coin == 5:
+        return 10
+    elif coin == 10:
+        return 25
+
+def next_smaller_coin(coin):
+    """Returns the next smaller coin in order.
+    >>> next_smaller_coin(25)
+    10
+    >>> next_smaller_coin(10)
+    5
+    >>> next_smaller_coin(5)
+    1
+    >>> next_smaller_coin(2) # Other values return None
+    """
+    if coin == 25:
+        return 10
+    elif coin == 10:
+        return 5
+    elif coin == 5:
+        return 1
+
+def count_coins(total):
+    """Return the number of ways to make change using coins of value of 1, 5, 10, 25.
+    >>> count_coins(15)
+    6
+    >>> count_coins(10)
+    4
+    >>> count_coins(20)
+    9
+    >>> count_coins(100) # How many ways to make change for a dollar?
+    242
+    >>> count_coins(200)
+    1463
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'count_coins', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    def constrained_count(total, smallest_coin):
+        if total == 0:
+            return 1
+        if total < 0:
+            return 0
+        if smallest_coin == None:
+            return 0
+        without_coin = constrained_count(total, next_larger_coin(smallest_coin))
+        with_coin = constrained_count(total - smallest_coin, smallest_coin)
+        return without_coin + with_coin
+    return constrained_count(total, 1)
 
